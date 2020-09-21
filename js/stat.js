d52e10f5220b14f45fbcf95b98fa5d0daa8ff73c
cerasus04 <cerasus1004@gmail.com>
2020-09-21 11:20:24 +0300

    Добработала гистограмму

--- js/stat.js
+++ js/stat.js
@@ -4,26 +4,27 @@ var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
+var CLOUD_COLOR = rgb(255, 255, 255);
+var SHADOW_X = 110;
+var SHADOW_Y = 20;
+var SHADOW_COLOR = rgba(0, 0, 0, 0.7);
 var GAP = 10;
-var FONT_GAP = 15;
-var TEXT_WIDTH = 50;
+var CONTENT_COLOR = rgb(0, 0, 0, 0);
+var TEXT_WIDTH = 40; // ширина области подписи, как и ширина столбиков
+var CONTENT_GAP = 50; // расстояние между столбиками и подписями
+var BAR_WIDTH = 40;
 var BAR_MAX_HEIGHT = 150;
+var CONTENT_X = BAR_WIDTH + CONTENT_GAP; //начало текста по х = сдвиг окна по х + расстояние между именами
+var MESSAGE_HEIGHT = 50;
+var TEXT_HEIGHT = 20;
 
 
-var CONTENT_GAP = 50; //расстояние между именами
-var CONTENT_X = CLOUD_X + CONTENT_GAP; //начало текста по х = сдвиг окна по х + расстояние между именами
-var CONTENT_Y = CLOUD_Y + CONTENT_GAP; // начало текста по у = сдвиг окна по y + расстояние между именами
-
-//var BAR_MAX_HEIGHT = 150;
-
-var barWidth = 40;
-
-var renderCloud = function (ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) { // рисуем облако
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function (arr) {
+var getMaxElement = function (arr) { //ищем максимальное значение
   var maxElement = arr[0];
 
   for (var i = 1; i < arr.length; i++) {
@@ -31,41 +32,62 @@ var getMaxElement = function (arr) {
       maxElement = arr[i];
     }
   }
-
   return maxElement;
+};
+
+var setFontStyle = function (ctx, font, baseline, style) { //Надпись на поле
+  ctx.font = font;
+  ctx.textBaseline = baseline;
+  ctx.fillStyle = style;
 };
 
 window.renderStatistics = function (ctx, players, times) { //отрисовка окна
   renderCloud(
     ctx,
-    CLOUD_X + GAP,
-    CLOUD_Y + GAP,
-    'rgba(0, 0, 0, 0.3)' //тень
+    SHADOW_X,
+    SHADOW_Y,
+    SHADOW_COLOR //тень
   );
+
   renderCloud( //белое окно
     ctx,
     CLOUD_X,
     CLOUD_Y,
-    '#fff'
+    CLOUD_COLOR
   );
 
   ctx.fillStyle = '#000'; //цвет заливки уменя должен будет меняться
+  ctx.font = `16px PT Mono`;
+  ctx.textBaseline = `hanging`;
+  ctx.fillText(`Ура вы победили!`, CLOUD_X + GAP * 2, CLOUD_Y + GAP * 2);
+  ctx.fillText(`Список результатов:`, CLOUD_X + GAP * 2, CLOUD_Y + GAP * 4);
 
-  var maxTime = getMaxElement(times); //максимальное время, чтобы понять, кто победил
+  var maxTime = getMaxElement(times);
 
   for (var i = 0; i < players.length; i++) { // Цикл в котором перебераются значения очков игроков для отрисовки имени
-    //певый элемент 0, пока индекс элемента меньше индекса последнего элемента массива, шаг 1
-    ctx.fillText(//отрисовка строки текст
-      players[i],
-      CONTENT_X + (barWidth + TEXT_WIDTH) * i,//координата по х, где начиается имя игрока
-      CLOUD_HEIGHT - GAP //координата по у, где начинается имя игрока
+    ctx.fillText( //текст
+      Math.round(times[i]),
+      CLOUD_X + BAR_WIDTH + CONTENT_X * i,
+      CLOUD_Y + MESSAGE_HEIGHT + GAP + BAR_MAX_HEIGHT - BAR_MAX_HEIGHT * times[i] / maxTime
     );
-
-    ctx.fillRect( //рисует прямоугольник с заливкой
-      CONTENT_X + (barWidth + TEXT_WIDTH) * i,// координата по х
-      CONTENT_Y - FONT_GAP, //координата по у
-      barWidth, //ширина
-      (BAR_MAX_HEIGHT * times[i]) / maxTime//высота
+    ctx.fillText( //гистограмма
+      players[i],
+      CLOUD_X + BAR_WIDTH + CONTENT_X * i,
+      CLOUD_Y + MESSAGE_HEIGHT + GAP * 2 + TEXT_HEIGHT + BAR_MAX_HEIGHT
     );
   }
-};
+
+  for (let j = 0; j < players.length; j++) {
+    if (players[j] === `Вы`) {
+      ctx.fillStyle = `rgba(255, 0, 0, 1)`;
+    } else {
+      ctx.fillStyle = `hsl(240, ` + Math.random() * 100 + `% , 50%)`;
+    }
+    ctx.fillRect(
+      CLOUD_X + BAR_WIDTH + CONTENT_X * j,
+      CLOUD_Y + MESSAGE_HEIGHT + GAP + TEXT_HEIGHT + BAR_MAX_HEIGHT - BAR_MAX_HEIGHT * times[j] / maxTime,
+      BAR_WIDTH,
+      BAR_MAX_HEIGHT * times[j] / maxTime
+    );
+  }
+}
