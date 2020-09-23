4e55805cb914ceb6cf5b5eb5bbd68efc27b35704
cerasus04 <cerasus1004@gmail.com>
2020-09-23 17:33:41 +0300

    Поправила расчеты

--- js/stat.js
+++ js/stat.js
@@ -5,11 +5,10 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 10;
-var CONTENT_GAP = 20;
 var TEXT_WIDTH = 50;
 var BAR_WIDTH = 40;
 var BAR_MAX_HEIGHT = 150;
-var CONTENT_X = CLOUD_X + CONTENT_GAP;
+var CONTENT_X = CLOUD_X + BAR_WIDTH;
 var CLOUD_COLOR = 'rgb(255, 255, 255)';
 var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
 var CONTENT_OFFSET_X = CLOUD_X + GAP * 2;
@@ -48,11 +47,12 @@ var drawHistogram = function (ctx, names, times) {
     var time = Math.floor(times[index]);
     var barX = CONTENT_X + (BAR_WIDTH + TEXT_WIDTH) * index;
     var barHeight = (BAR_MAX_HEIGHT * time) / maxTime;
-    var barY = CLOUD_HEIGHT - CONTENT_GAP - GAP - barHeight;
+    var barY = CLOUD_HEIGHT - GAP * 2 - barHeight;
+
     setFontStyle(ctx, '16px PT Mono', 'hanging', 'rgb(0, 0, 0)');
 
-    ctx.fillText(name, barX, CLOUD_HEIGHT - CONTENT_GAP);
-    ctx.fillText(time, barX, barY - CONTENT_GAP);
+    ctx.fillText(name, barX, CLOUD_HEIGHT - GAP);
+    ctx.fillText(time, barX, barY - GAP * 2);
     ctx.fillStyle = name === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(240, ' + Math.random() * 100 + '% , 50%)';
     ctx.fillRect(barX, barY, BAR_WIDTH, barHeight);
   });
